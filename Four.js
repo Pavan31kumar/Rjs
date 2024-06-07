652592f7a90063e2fe92dd528c946ccc701d0fcf
Pavan31kumar <158801173+Pavan31kumar@users.noreply.github.com>
2024-06-07 07:10:04 +0000

    Multiple Array

--- Four.js
+++ Four.js
@@ -1,3 +1,20 @@
 //  Multiple Array In JavaScript
- var marks=new Array(3)
- console.log('Size of Marks '+marks.length);
+var marks = new Array(3);
+for (i = 0; i < marks.length; i++) {
+  marks[i] = new Array(6);
+}
+console.log("Size of Marks " + marks.length);
+for (i = 0; i < marks.length; i++) {
+  console.log(marks[i].length);
+}
+for (i = 0; i < marks.length; i++) {
+  for (j = 0; j < marks[i].length; j++) {
+    marks[i][j] = i * j;
+  }
+}
+for (i = 0; i < marks.length; i++) {
+  for (j = 0; j < marks[i].length; j++) {
+    console.log(marks[i][j]);
+  }
+}
+console.log(marks);
