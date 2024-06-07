e9cf8a967278eb8b6aff620ffb41f770aca77959
Pavan31kumar <158801173+Pavan31kumar@users.noreply.github.com>
2024-06-07 14:07:57 +0000

    Differt types of Functions in JavaScript

--- Two.js
+++ Two.js
@@ -12,3 +12,23 @@ add(true,false);
 add('a'+'d');
 add(11.2325,12.23);
 //  Add(12,52);                                 //Add not Defined
+
+// Different Types of Functions in JavaScript
+ function show(a,d){
+  console.log('I am From Show function');
+ }
+ show();
+ var display=show();
+ console.log(display);
+ var Fn=function (){
+  console.log('I am from var fn');
+ }
+ console.log(Fn);
+  var FnA=()=>{                                 // Arrow Function
+  console.log('I am from var fnA');
+ }
+ console.log(FnA);
+ 
+ (function add(a,b){                             // direct operation without add 
+  console.log(a+b);
+ }(20,30));
