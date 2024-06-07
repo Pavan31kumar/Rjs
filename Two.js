3b972e0ad8f28645bea5f92384d9258cdc79a858
Pavan31kumar <158801173+Pavan31kumar@users.noreply.github.com>
2024-06-07 12:50:25 +0000

    Function all Operations

--- Two.js
+++ Two.js
@@ -2,8 +2,13 @@
 function add(a, b) {
   console.log(a + "   " + typeof (a));
   console.log(b + "  " + typeof (b));
-  // console.log(c + "   " + typeof c);
+ //  console.log(c + "   " + typeof c);     // c is not defined in Function
   console.log(a + b);
 }
 add(12, 20);
-add(12, 23, 26);
+add(12, 23, 26);                            // c is not defined
+add('Sudheer','Pavan');
+add(true,false);
+add('a'+'d');
+add(11.2325,12.23);
+//  Add(12,52);                                 //Add not Defined
