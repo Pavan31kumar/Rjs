b8ea6dc97fe1d0fc748d55ddc51b2cc959063cfd
Pavan31kumar <158801173+Pavan31kumar@users.noreply.github.com>
2024-06-07 12:39:15 +0000

    Functions

---
+++ Two.js
@@ -0,0 +1,9 @@
+//  Functons....
+function add(a, b) {
+  console.log(a + "   " + typeof (a));
+  console.log(b + "  " + typeof (b));
+  // console.log(c + "   " + typeof c);
+  console.log(a + b);
+}
+add(12, 20);
+add(12, 23, 26);
